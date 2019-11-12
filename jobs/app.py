b776466df354f954eabde88625cf7517d5281b7e
Cosmin Ianculescu <cosminianculescu91@gmail.com>
2019-11-12 15:52:01 +0200

    module5

--- jobs/app.py
+++ jobs/app.py
@@ -23,6 +23,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
     cursor.close()
     return results
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
@@ -35,3 +36,9 @@ def close_connection(exception):
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
+
+
+@app.route('/job/<job_id>')
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
+    return render_template('job.html', job=job)
